1e273cf2f8634bf2b66428d21ce35a628ea8ddf6
mittiii <srp5628comust@gmail.com>
2024-10-19 16:42:44 +0900

    関数を分割

--- server/main.go
+++ server/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -46,52 +42,4 @@ func main() {
 	})
 
 	router.Run(":8080")
-}
-
-func getOpenAIResponse(apiKey string) OpenaiResponse {
-	requestBody := OpenaiRequest{
-		Model:    "gpt-3.5-turbo",
-		Messages: messages,
-	}
-
-	requestJSON, _ := json.Marshal(requestBody)
-
-	req, err := http.NewRequest("POST", openaiURL, bytes.NewBuffer(requestJSON))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(resp.Body)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var response OpenaiResponse
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		println("Error: ", err.Error())
-		return OpenaiResponse{}
-	}
-
-	messages = append(messages, Message{
-		Role:    "assistant",
-		Content: response.Choices[0].Messages.Content,
-	})
-
-	return response
 }
